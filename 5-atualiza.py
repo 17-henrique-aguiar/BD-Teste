9204cb337c0522e891a1389f2f54569eb28ebd9a
Henrique <henriqueaguiar670@gmail.com>
2025-05-17 04:33:09 +0000

    a

--- 5-atualiza.py
+++ 5-atualiza.py
@@ -4,13 +4,13 @@ conexao = sqlite3.connect('titulo.db')
 cursor = conexao.cursor()
 
 # Atualizando dados 
-id = 2
+id = 1
 cursor.execute(
     """
         UPDATE filmes SET nome = ?
         WHERE id = ?
     """,
-    ("Homem Aranha", id)
+    ("Sonic", id)
 )
 
 conexao.commit()
